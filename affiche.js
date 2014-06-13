148661cd100eaaed11d0821d50ba0bce1def12a0
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-06-13 23:10:35 +0200

    Externalise l'affichage

---
+++ affiche.js
@@ -0,0 +1,20 @@
+function afficheTexte(texte, ensemble)
+{
+	sortie = document.getElementById('affiche');
+
+	sortie.innerHTML += texte + ' : [';
+
+	if(ensemble.cartes.length > 0)
+	{
+		sortie.innerHTML += ensemble.cartes[0].id;
+
+		for(i = 1; i < ensemble.cartes.length; i++)
+		{
+			sortie.innerHTML += ' ' + ensemble.cartes[i].id;
+		}
+	}
+
+	sortie.innerHTML += ']<br \/\><br \/\>';
+}
+
+
